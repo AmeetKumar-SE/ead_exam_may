00ed3220b2a7bdc243a245409832437d503f827a
AmeetKumar-SE <sonusachdev23@gmail.com>
2023-05-27 17:38:18 +0500

    Add PollDisplay Component

--- src/App.js
+++ src/App.js
@@ -3,63 +3,24 @@ import { Route, BrowserRouter, Routes } from 'react-router-dom';
 import './App.css';
 import TestCom from './testComponent';
 import Timer from './TaskBFile';
-import { PollDisplay } from './TaskAFile';
+import UserParti, { PollDisplay } from './TaskAFile';
+import { useState } from 'react';
 
 
 function App() {
+
   return (
-    //className="App"
     <div>
+       <Routes>
+        <Route path='/' element={<PollDisplay/>} />
+        <Route path='/context' element={<UserParti/>} />
+      </Routes>
       
-      
-      <Timer/>
+      {/* <Timer/> */}
+
       {/* <PollDisplay/> */}
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
     </div>
   );
 }
 
 export default App;
-
-
-
-
-// import './App.css';
-
-// import {  Routes, Route } from 'react-router-dom';
-// import Forms from './CRUD/Forms';
-// import Showdata from './CRUD/Showdata'
-// import { useState } from 'react';
-
-
-// function App() {
-//   const [array, setArray] = useState([]);
-//   const addDataToArr = (obj) => {
-//     setArray(prevArray => [...prevArray, obj]);
-//   }
-
-//   return (<>
-//     <Routes>
-//       <Route index path='/' element={<Forms arr={addDataToArr}/>} />
-//       <Route path='showpage' element={<Showdata arr={array}/>} />
-//     </Routes>
-//     {/* <ComponentDidMount initial={1} min={1} max={10}/> */}
-//     {/* <FetchData/> */}
-//     </>   
-//   );
-// }
-
-// export default App;
--- src/TaskAFile.js
+++ src/TaskAFile.js
@@ -1,19 +1,60 @@
-// import React, { useState } from "react";
+import { Button } from "bootstrap";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-// export function PollDisplay(){
+const data = {
 
-//     const [voteJavaScript, setVoteJavaScript] = useState(0)
-//     const [votePython, setVotePython] = useState(0)
-//     const [voteJava, setVoteJava] = useState(0)
-//     const [voteC, setVoteC] = useState(0)
+    "question": "What is your favorite programming language?",
+  
+    "choices": [
+  
+      { "id": 1, "label": "JavaScript", "votes": 0 },
+  
+      { "id": 2, "label": "Python", "votes": 0 },
+  
+      { "id": 3, "label": "Java", "votes": 0 },
+  
+      { "id": 4, "label": "C#", "votes": 0 }
+  
+    ]
+}
+
+export function PollDisplay(){
+
+    const navigate = useNavigate()
+
+    return<>
+    <div>Question: {data.question}</div>
+    {data.choices.map((data2)=> <li key={data2.id}>{data2.label} : Total Votes till {data2.votes}</li>)}
+    <button onClick={()=>{navigate('/context')}}>Enter Context</button>
+    </>
+}
+
+export default function UserParti(){
+
+    // const [votes, setVotes] = useState({
+
+    // })
+
+    function voteClick(choiceId) {
+        // setVotes(data.choices[choiceId] + 1);
+        // console.log(votes)
+    }
 
 
-//     return<>
-//     <p>What is your favorite programming language?</p>
-//     <button onClick={()=>{setVoteJavaScript(voteJavaScript+1)}}>JavaScript</button>
-//     <button>Python</button>
-//     <button>Java</button>
-//     <button>C#</button>
-    
-//     </>
-// }
+    return(<>
+    <h2>Inside User Participant</h2>
+    <form>
+    <div>Question: {data.question}</div>
+    {data.choices.map((data2)=> {return<div><input type="radio"></input><li key={data2.id} onClick={() => voteClick(data2.id)}>
+        {data2.label} : Total Votes: {data2.votes}
+        </li>
+        </div>})}
+
+        <button>Submit</button>
+        
+    </form>
+    </>)
+
+
+}
